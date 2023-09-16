2094b715ceab80b32325e50b08caa3171dd214b7
Alexxeev <alexeev.dev@yandex.ru>
2023-09-16 22:36:11 +0300

    Update API URL string

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   runApp(const MyApp());
 }
 
-const String baseUrl = 'base_url_template';
+const String baseUrl = 'http://base_url_template';
 
 class CustomScrollBehavior extends MaterialScrollBehavior {
   @override
