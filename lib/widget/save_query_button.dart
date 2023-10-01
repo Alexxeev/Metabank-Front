e12a404ec31d501ccd71e86400725431771c12a0
Alexxeev <alexeev.dev@yandex.ru>
2023-10-01 21:50:15 +0300

    Change save query button to extended one

--- lib/widget/save_query_button.dart
+++ lib/widget/save_query_button.dart
@@ -6,19 +6,21 @@ class SaveQueryButton extends StatelessWidget {
   const SaveQueryButton(this._metadataRepository, {super.key});
 
   final MetadataRepository _metadataRepository;
-  
+
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-        onPressed: () async {
-          await _metadataRepository.saveQuery(queryModel: CurrentPage.instance.lastUpdate!);
-          // ignore: use_build_context_synchronously
-          if (!context.mounted) return;
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Query saved')));
-        },
-        tooltip: 'Save current query',
-        child: const Icon(Icons.save),
-      );
+    return FloatingActionButton.extended(
+      onPressed: () async {
+        await _metadataRepository.saveQuery(
+            queryModel: CurrentPage.instance.lastUpdate!);
+        // ignore: use_build_context_synchronously
+        if (!context.mounted) return;
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('Query saved')));
+      },
+      tooltip: 'Save current query',
+      icon: const Icon(Icons.save),
+      label: const Text('Save query'),
+    );
   }
-  
 }
